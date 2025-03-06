7b678805aa57aaa77d95ffbb4b10af0e0cea8640
Dyego Caldeira <dyegocaldeira@gmail.com>
2025-03-05 22:04:02 -0300

    fix(producer): fix multiple movies result by producer

--- src/producer/producer.service.ts
+++ src/producer/producer.service.ts
@@ -1,114 +1,53 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { ProducerEnum } from './producer.enum';
-import { Repository } from 'typeorm';
 import { Producers } from './producer.entity';
+import { IProducerAwards, IProducerRepository, IResultsProducerAwards } from './producer.interface';
 
 @Injectable()
 export class ProducerService {
   constructor(
     @Inject(ProducerEnum.PRODUCER_REPOSITORY)
-    private readonly producerRepository: Repository<Producers>,
-  ) {}
-
-  public async saveProducer(producer: Producers): Promise<Producers> {
-    return this.producerRepository.save(producer);
-  }
+    private readonly producerRepository: IProducerRepository
+  ) { }
 
   public async upsertProducer(producer: Producers): Promise<Producers> {
-    const databaseProducer = await this.producerRepository.findOne({
-      where: {
-        name: producer.name,
+    return (await this.producerRepository.findByName(producer.name)) ?? this.producerRepository.save(producer);
+  }
+
+  public async getProducerWithAwardRange(): Promise<IResultsProducerAwards> {
+    const databaseWinnerProducers = await this.producerRepository.findWinningProducers();
+
+    const producerAwards = this.calculateAwardIntervals(databaseWinnerProducers);
+    const awardIntervals = [...new Set(producerAwards.map(({ interval }) => interval))];
+    const [minInterval, maxInterval] = [Math.min(...awardIntervals), Math.max(...awardIntervals)];
+
+    return this.filterAwardsByInterval(minInterval, maxInterval, producerAwards);
+  }
+
+  private filterAwardsByInterval(minInterval: number, maxInterval: number, producerAwards: IProducerAwards[]): IResultsProducerAwards {
+
+    return producerAwards.reduce(
+      (acc, award) => {
+        if (award.interval === minInterval) acc.min.push(award);
+        if (award.interval === maxInterval) acc.max.push(award);
+        return acc;
       },
+      { min: [], max: [] } as IResultsProducerAwards
+    );
+  }
+
+  private calculateAwardIntervals(winnerProducers: Producers[]): IProducerAwards[] {
+
+    return winnerProducers.flatMap(({ name, movies }) => {
+      if (movies.length < 2) return [];
+
+      const sortedMovies = [...movies].sort((a, b) => a.year - b.year);
+      return sortedMovies.slice(1).map((movie, i) => ({
+        producer: name,
+        interval: movie.year - sortedMovies[i].year,
+        previousWin: sortedMovies[i].year,
+        followingWin: movie.year,
+      }));
     });
-
-    return databaseProducer ?? this.producerRepository.save(producer);
-  }
-
-  async findAll(): Promise<Producers[]> {
-    return this.producerRepository
-      .createQueryBuilder('producer')
-      .leftJoinAndSelect('producer.movies', 'movie')
-      .where('movie.winner = :winner', { winner: true })
-      .getMany();
-  }
-
-  public async getProducerWithAwardRange(): Promise<ResultsProducerAwards> {
-    const databaseWinnerProducers = await this.producerRepository
-      .createQueryBuilder('producer')
-      .leftJoinAndSelect('producer.movies', 'movie')
-      .where('movie.winner = :winner', { winner: true })
-      .getMany();
-
-    const resultsProducerAwards: ResultsProducerAwards = {
-      min: [],
-      max: [],
-    };
-
-    resultsProducerAwards.min = this.getMinRange(databaseWinnerProducers);
-    resultsProducerAwards.max = this.getMaxRange(databaseWinnerProducers);
-
-    return resultsProducerAwards;
-  }
-
-  private getMinRange(winnerProducers: Producers[]): ProducerAwards[] {
-    const producerAwards = this.calculateProducerAwards(
-      winnerProducers,
-      (a, b) => a < b,
-      Number.MAX_SAFE_INTEGER,
-    );
-    const minInterval = Math.min(
-      ...producerAwards.map((producer) => producer.interval),
-    );
-    return producerAwards.filter(
-      (producer) => producer.interval === minInterval,
-    );
-  }
-
-  private getMaxRange(winnerProducers: Producers[]): ProducerAwards[] {
-    const producerAwards = this.calculateProducerAwards(
-      winnerProducers,
-      (a, b) => a > b,
-      0,
-    );
-    const maxInterval = Math.max(
-      ...producerAwards.map((producer) => producer.interval),
-    );
-
-    return producerAwards.filter(
-      (producer) => producer.interval === maxInterval,
-    );
-  }
-
-  private calculateProducerAwards(
-    winnerProducers: Producers[],
-    comparator: (a: number, b: number) => boolean,
-    initialValue: number,
-  ): ProducerAwards[] {
-    const results = winnerProducers
-      .filter((producer) => producer.movies.length > 1)
-      .map((producer) => {
-        const producerMovies = producer.movies.sort((a, b) => a.year - b.year);
-        let bestInterval = initialValue;
-        let previousWin = 0;
-        let followingWin = 0;
-
-        for (let i = 1; i < producerMovies.length; i++) {
-          const interval = producerMovies[i].year - producerMovies[i - 1].year;
-          if (comparator(interval, bestInterval)) {
-            bestInterval = interval;
-            previousWin = producerMovies[i - 1].year;
-            followingWin = producerMovies[i].year;
-          }
-        }
-
-        return {
-          producer: producer.name,
-          interval: bestInterval,
-          previousWin,
-          followingWin,
-        };
-      });
-
-    return results;
   }
 }
